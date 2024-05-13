d43ac8fae08d5a4afc64592fb62290507d4f38e7
abnersuecia <zarabnersuecia@gmail.com>
2024-05-13 13:03:45 -0300

    1.3primeira versão funcionando

--- src/uiprincipal.java
+++ src/uiprincipal.java
@@ -1,18 +1,25 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class uiprincipal {
+public class uiprincipal extends JFrame{
     private JLabel login;
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JButton criarContaButton;
     private JButton loginButton;
     private JButton cancelarButton;
+    private JPanel painelLogin;
 
 
-
+    public static void main(String[] args) {
+        uiprincipal u = new uiprincipal();
+    }
 
     public uiprincipal() {
+        setSize(400,400);
+        setContentPane(painelLogin);
+        setVisible(true);
+
         criarContaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
